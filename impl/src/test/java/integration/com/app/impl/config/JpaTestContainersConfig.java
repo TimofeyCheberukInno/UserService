ff1b9f6679b93c5e88e655d24afc8c9c820afa8d
TimofeyCheberukInno <timofey13052005@gmail.com>
2025-08-23 14:23:29 +0300

    test(integration/*): added dependencies for Testcontainers. implemented configurations for integration tests.

---
+++ impl/src/test/java/integration/com/app/impl/config/JpaTestContainersConfig.java
@@ -0,0 +1,27 @@
+package integration.com.app.impl.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@TestConfiguration
+@Profile("jpa")
+@Testcontainers
+public class JpaTestContainersConfig {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
