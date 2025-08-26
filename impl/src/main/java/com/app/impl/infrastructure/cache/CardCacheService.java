9311bd247af8ee1505bec3174987328bc5bbff08
TimofeyCheberukInno <tsimafei.chabiaruk@innowise.com>
2025-08-26 15:36:37 +0300

    feat(UserCacheService/CardCacheService): implemented deleteAll() methods in cache services

--- impl/src/main/java/com/app/impl/infrastructure/cache/CardCacheService.java
+++ impl/src/main/java/com/app/impl/infrastructure/cache/CardCacheService.java
@@ -20,76 +20,83 @@ import com.app.impl.dto.cardDtos.CardWithUserDto;
 @Service
 @Slf4j
 public class CardCacheService {
-	private final RedisTemplate<String, Object> redisTemplate;
-	private static final int TIME_TO_LIVE_FOR_CARD_IN_MINUTES = 60;
-	private static final String CARDS_CACHE_NAME = "cards";
-	private static final String CARDS_WITH_USER_CACHE_NAME = "cards-with-user";
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final int TIME_TO_LIVE_FOR_CARD_IN_MINUTES = 60;
+    private static final String CARDS_CACHE_PREFIX = "cards";
+    private static final String CARDS_WITH_USER_CACHE_PREFIX = "cards-with-user";
 
-	@Autowired
-	public CardCacheService(RedisTemplate<String, Object> redisTemplate) {
-		this.redisTemplate = redisTemplate;
-	}
+    @Autowired
+    public CardCacheService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
-	public void saveCardWithoutUser(CardDto card) {
-		redisTemplate.opsForValue().set(makeKey(CARDS_CACHE_NAME, card.id()), card,
-				Duration.ofMinutes(TIME_TO_LIVE_FOR_CARD_IN_MINUTES));
-		log.info("Putting cache for {} with id {}!", CARDS_CACHE_NAME, card.id());
-	}
+    public void saveCardWithoutUser(CardDto card) {
+        redisTemplate.opsForValue().set(makeKey(CARDS_CACHE_PREFIX, card.id()), card,
+                Duration.ofMinutes(TIME_TO_LIVE_FOR_CARD_IN_MINUTES));
+        log.info("Putting cache for {} with id {}!", CARDS_CACHE_PREFIX, card.id());
+    }
 
-	public void saveCardWithUser(CardWithUserDto card) {
-		redisTemplate.opsForValue().set(makeKey(CARDS_WITH_USER_CACHE_NAME, card.id()), card,
-				Duration.ofMinutes(TIME_TO_LIVE_FOR_CARD_IN_MINUTES));
-		log.info("Putting cache for {} with id {}!", CARDS_WITH_USER_CACHE_NAME, card.id());
-	}
+    public void saveCardWithUser(CardWithUserDto card) {
+        redisTemplate.opsForValue().set(makeKey(CARDS_WITH_USER_CACHE_PREFIX, card.id()), card,
+                Duration.ofMinutes(TIME_TO_LIVE_FOR_CARD_IN_MINUTES));
+        log.info("Putting cache for {} with id {}!", CARDS_WITH_USER_CACHE_PREFIX, card.id());
+    }
 
-	public void updateCardWithoutUser(CardDto card) {
-		saveCardWithoutUser(card);
-		log.info("Updating cache for {} with id {}...", CARDS_CACHE_NAME, card.id());
-	}
+    public void updateCardWithoutUser(CardDto card) {
+        saveCardWithoutUser(card);
+        log.info("Updating cache for {} with id {}...", CARDS_CACHE_PREFIX, card.id());
+    }
 
-	public void updateCardWithUser(CardWithUserDto card) {
-		saveCardWithUser(card);
-		log.info("Updating cache for {} with id {}...", CARDS_WITH_USER_CACHE_NAME, card.id());
-	}
+    public void updateCardWithUser(CardWithUserDto card) {
+        saveCardWithUser(card);
+        log.info("Updating cache for {} with id {}...", CARDS_WITH_USER_CACHE_PREFIX, card.id());
+    }
 
-	public Optional<CardWithUserDto> getByIdWithUser(Long id) {
-		Object object = redisTemplate.opsForValue().get(makeKey(CARDS_WITH_USER_CACHE_NAME, id));
-		if (object instanceof CardWithUserDto) {
-			log.info("Cache hit for {} with id {}!", CARDS_WITH_USER_CACHE_NAME, id);
-			return Optional.of((CardWithUserDto) object);
-		}
-		log.info("Cache miss for {} with id {}!", CARDS_WITH_USER_CACHE_NAME, id);
+    public Optional<CardWithUserDto> getByIdWithUser(Long id) {
+        Object object = redisTemplate.opsForValue().get(makeKey(CARDS_WITH_USER_CACHE_PREFIX, id));
+        if (object instanceof CardWithUserDto) {
+            log.info("Cache hit for {} with id {}!", CARDS_WITH_USER_CACHE_PREFIX, id);
+            return Optional.of((CardWithUserDto) object);
+        }
+        log.info("Cache miss for {} with id {}!", CARDS_WITH_USER_CACHE_PREFIX, id);
 
-		return Optional.empty();
-	}
+        return Optional.empty();
+    }
 
-	public List<CardDto> getByIdsWithoutUser(Collection<Long> ids) {
-		List<CardDto> cachedCards = getAndValidateObjectsFromCache(redisTemplate, CARDS_CACHE_NAME, ids, CardDto.class);
+    public List<CardDto> getByIdsWithoutUser(Collection<Long> ids) {
+        List<CardDto> cachedCards = getAndValidateObjectsFromCache(redisTemplate, CARDS_CACHE_PREFIX, ids, CardDto.class);
 
-		List<Long> cachedCardsIds = cachedCards.stream()
+        List<Long> cachedCardsIds = cachedCards.stream()
                 .map(CardDto::id)
                 .toList();
-		log.info("Cache hit for {} with ids {}!", CARDS_CACHE_NAME, cachedCardsIds);
+        log.info("Cache hit for {} with ids {}!", CARDS_CACHE_PREFIX, cachedCardsIds);
 
-		return cachedCards;
-	}
+        return cachedCards;
+    }
 
-	public List<CardWithUserDto> getByIdsWithUser(Collection<Long> ids) {
-		List<CardWithUserDto> cachedCards =
-                getAndValidateObjectsFromCache(redisTemplate, CARDS_WITH_USER_CACHE_NAME, ids, CardWithUserDto.class);
+    public List<CardWithUserDto> getByIdsWithUser(Collection<Long> ids) {
+        List<CardWithUserDto> cachedCards =
+                getAndValidateObjectsFromCache(redisTemplate, CARDS_WITH_USER_CACHE_PREFIX, ids, CardWithUserDto.class);
 
-		List<Long> cachedCardsIds = cachedCards.stream()
+        List<Long> cachedCardsIds = cachedCards.stream()
                 .map(CardWithUserDto::id)
                 .toList();
-		log.info("Cache hit for {} with ids {}!", CARDS_CACHE_NAME, cachedCardsIds);
+        log.info("Cache hit for {} with ids {}!", CARDS_CACHE_PREFIX, cachedCardsIds);
 
-		return cachedCards;
-	}
+        return cachedCards;
+    }
 
-	public void delete(Long id) {
-		redisTemplate.delete(makeKey(CARDS_CACHE_NAME, id));
-		log.info("Evicting cache for {} with id {}!", CARDS_CACHE_NAME, id);
-		redisTemplate.delete(makeKey(CARDS_WITH_USER_CACHE_NAME, id));
-		log.info("Evicting cache for {} with id {}!", CARDS_WITH_USER_CACHE_NAME, id);
-	}
+    public void delete(Long id) {
+        redisTemplate.delete(makeKey(CARDS_CACHE_PREFIX, id));
+        log.info("Evicting cache for {} with id {}!", CARDS_CACHE_PREFIX, id);
+        redisTemplate.delete(makeKey(CARDS_WITH_USER_CACHE_PREFIX, id));
+        log.info("Evicting cache for {} with id {}!", CARDS_WITH_USER_CACHE_PREFIX, id);
+    }
+
+    public void deleteAll() {
+        var keys = redisTemplate.keys(CARDS_CACHE_PREFIX + ":*");
+        if(keys != null && !keys.isEmpty()){
+            redisTemplate.delete(keys);
+        }
+    }
 }
