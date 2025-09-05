0ff4b0d3d022bd556de962de25154d41e388b314
TimofeyCheberukInno <tsimafei.chabiaruk@innowise.com>
2025-09-05 23:47:17 +0300

    chore: increased redis timeout

--- impl/src/test/java/com/app/impl/integration/config/TestContainersConfig.java
+++ impl/src/test/java/com/app/impl/integration/config/TestContainersConfig.java
@@ -1,18 +1,21 @@
 package com.app.impl.integration.config;
 
+import java.time.Duration;
+
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import com.redis.testcontainers.RedisContainer;
-
+i
 @TestConfiguration
 public class TestContainersConfig {
     @Bean
     @ServiceConnection
     public RedisContainer redisContainer() {
-        return new RedisContainer("redis:7.0-alpine");
+        return new RedisContainer("redis:7.0-alpine")
+                .withStartupTimeout(Duration.ofMinutes(5));
     }
 
     @Bean
